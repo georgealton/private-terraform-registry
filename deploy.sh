f0a340c0392e92e16a1a1b9ff8700c64f76c57e7
George Alton <georgealton@gmail.com>
2022-06-16 00:32:50 +0100

    webhook parsing

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,15 @@ aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --no-fail-on-empty-changeset
 
+BASE_URL=https://i69k0wcgvg.execute-api.eu-west-1.amazonaws.com
+
 # When we list versions for a module
 #     the api returns a list of versions
-curl -q https://i69k0wcgvg.execute-api.eu-west-1.amazonaws.com/main/terraform/modules/v1/A/B/C/versions | jq
+curl -q $BASE_URL/main/terraform/modules/v1/A/B/C/versions | jq
 # When we request to download a version of a modules
 # The API returns terraform protocol
-curl -v https://i69k0wcgvg.execute-api.eu-west-1.amazonaws.com/main/terraform/modules/v1/A/B/C/1.0.0/download
+curl -v $BASE_URL/main/terraform/modules/v1/A/B/C/1.0.0/download
 # The API returns a 404 and errors if the version does not exist
-curl -v https://i69k0wcgvg.execute-api.eu-west-1.amazonaws.com/main/terraform/modules/v1/A/B/C/0.0.0/download
+curl -v $BASE_URL/main/terraform/modules/v1/A/B/C/0.0.0/download
+
+curl -v -H 'x-github-event:repository' -H 'content-type:application/json' -d @data/github-repository-created.json $BASE_URL/main/webhooks/github
