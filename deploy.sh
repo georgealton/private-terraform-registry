f209ef2b32edd496df64cc93a8a4627a2d0617ad
George Alton <georgealton@gmail.com>
2022-06-18 22:30:11 +0100

    asd

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
 aws cloudformation package \
-    --template template.yaml \
-    --s3-bucket cf-templates-1491x2vk47ot9-eu-west-1 \
-    --output-template-file packaged-template.json
+    --template 'template.yaml' \
+    --s3-bucket 'cf-templates-1491x2vk47ot9-eu-west-1' \
+    --output-template-file 'packaged-template.json'
 
 aws cloudformation deploy \
-    --template-file packaged-template.json \
-    --stack-name private-terraform-registry \
-    --capabilities CAPABILITY_NAMED_IAM \
+    --template-file 'packaged-template.json' \
+    --stack-name 'private-terraform-registry' \
+    --capabilities 'CAPABILITY_NAMED_IAM' \
     --disable-rollback \
     --no-fail-on-empty-changeset \
-    --parameter-overrides file://parameters.json
+    --parameter-overrides 'file://parameters.json'
 
-BASE_URL=https://terraform.georgealton.com
+BASE_URL='https://terraform.georgealton.com'
 
 ###
 echo "When we list versions for a module"
-echo "    The api returns a list of versions"
+echo "The api returns a list of versions"
 http --follow "${BASE_URL}/terraform/modules/v1/A/B/C/versions"
-echo "    The api returns 404 when there are no versions"
+echo "The api returns 404 when there are no versions"
 http --follow "${BASE_URL}/terraform/modules/v1/X/X/X/versions"
 ####
 
 ####
 echo "When we request to download a version of a modules"
-echo "   The API returns the X-TERRAFORM-GET header"
+echo "The API returns the X-TERRAFORM-GET header"
 http --follow "${BASE_URL}/terraform/modules/v1/A/B/C/1.0.0/download"
-echo "   The API returns a 404 and errors if the version does not exist"
+echo "The API returns a 404 and errors if the version does not exist"
 http --follow "${BASE_URL}/terraform/modules/v1/A/B/C/0.0.0/download"
 ####
 
