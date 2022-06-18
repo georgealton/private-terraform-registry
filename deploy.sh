06ef8b008bc104e4c4a504659c5757440552c4f6
George Alton <georgealton@gmail.com>
2022-06-18 22:26:44 +0100

    asdasd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 aws cloudformation package \
     --template template.yaml \
@@ -18,19 +18,19 @@ BASE_URL=https://terraform.georgealton.com
 ###
 echo "When we list versions for a module"
 echo "    The api returns a list of versions"
-http --session=session --follow "${BASE_URL}/terraform/modules/v1/A/B/C/versions"
+http --follow "${BASE_URL}/terraform/modules/v1/A/B/C/versions"
 echo "    The api returns 404 when there are no versions"
-http --session=session --follow "${BASE_URL}/terraform/modules/v1/X/X/X/versions"
+http --follow "${BASE_URL}/terraform/modules/v1/X/X/X/versions"
 ####
 
 ####
 echo "When we request to download a version of a modules"
 echo "   The API returns the X-TERRAFORM-GET header"
-http --session=session --follow "${BASE_URL}/terraform/modules/v1/A/B/C/1.0.0/download"
+http --follow "${BASE_URL}/terraform/modules/v1/A/B/C/1.0.0/download"
 echo "   The API returns a 404 and errors if the version does not exist"
-http --session=session --follow "${BASE_URL}/terraform/modules/v1/A/B/C/0.0.0/download"
+http --follow "${BASE_URL}/terraform/modules/v1/A/B/C/0.0.0/download"
 ####
 
-http --session=session --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github/tag-added.json'
-# http --session=session --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github-repository-created.json'
-# http --session=session --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:installation" "CONTENT-TYPE:application/json" '@data/github-app-installation.json'
+http --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github/tag-added.json'
+# http --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github-repository-created.json'
+# http --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:installation" "CONTENT-TYPE:application/json" '@data/github-app-installation.json'
