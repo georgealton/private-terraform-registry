d32c6ab8106491605f2dfd7c38ac6d2bfb3405db
George Alton <georgealton@gmail.com>
2022-07-01 12:51:32 +0100

    a

--- deploy.sh
+++ deploy.sh
@@ -14,19 +14,17 @@ aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --parameter-overrides 'file://parameters.json'
 
-BASE_URL='https://terraform.georgealton.com'
+BASE_URL="https://terraform.georgealton.com"
 
 ###
 echo "When we list versions for a module"
 echo "The api returns a list of versions"
-http \
-    --ignore-stdin \
+http --ignore-stdin \
     --timeout 5 \
     --follow \
     "${BASE_URL}/terraform/modules/v1/A/B/C/versions"
 echo "The api returns 404 when there are no versions"
-http \
-    --ignore-stdin \
+http --ignore-stdin \
     --timeout 5 \
     --follow \
     "${BASE_URL}/terraform/modules/v1/X/X/X/versions"
@@ -35,24 +33,22 @@ http \
 ####
 echo "When we request to download a version of a modules"
 echo "The API returns the X-TERRAFORM-GET header"
-http \
-    --ignore-stdin \
+http --ignore-stdin \
     --timeout 5 \
     --follow \
     "${BASE_URL}/terraform/modules/v1/A/B/C/1.0.0/download"
+
 echo "The API returns a 404 and errors if the version does not exist"
-http \
-    --ignore-stdin \
+http --ignore-stdin \
     --timeout 5 \
     --follow \
     "${BASE_URL}/terraform/modules/v1/A/B/C/0.0.0/download"
 ####
 
-http \
-    --ignore-stdin \
+http --ignore-stdin \
     --timeout 5 \
     --follow \
     "${BASE_URL}/webhooks/github" \
-    "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github/tag-added.json'
+    "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github/events/tag-added.json'
 # http --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:repository" "CONTENT-TYPE:application/json" '@data/github-repository-created.json'
 # http --follow "${BASE_URL}/webhooks/github" "X-GITHUB-EVENT:installation" "CONTENT-TYPE:application/json" '@data/github-app-installation.json'
