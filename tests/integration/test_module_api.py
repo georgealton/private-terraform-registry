cf4ffdd897a3232ff985f5db0a3d4dc93ee6b8d4
George Alton <georgealton@gmail.com>
2023-01-30 12:07:08 +0000

    jg

--- tests/integration/test_module_api.py
+++ tests/integration/test_module_api.py
@@ -1,29 +1,33 @@
+from mimetypes import types_map
+from http import HTTPStatus
 import requests
 
 BASE_URL = "https://terraform.georgealton.com"
+content_type = types_map[".json"]
 
 
 class TestServiceDiscovery:
     def test_returns_modules_url(self):
         url = f"{BASE_URL}/.well-known/terraform.json"
         response = requests.get(url)
-        assert response.status_code == 200
-        assert response.headers["Content-Type"] == "application/json"
-        assert response.json() == {"modules.v1": f"{BASE_URL}/terraform/modules/v1/"}
+        body = response.json()
+        assert response.status_code == HTTPStatus.OK
+        assert response.headers["Content-Type"] == content_type
+        assert body == {"modules.v1": f"{BASE_URL}/terraform/modules/v1/"}
 
 
 class TestListModuleVersions:
     def test_returns_a_list_of_versions(self):
         url = f"{BASE_URL}/terraform/modules/v1/A/B/C/versions"
         response = requests.get(url)
-        assert response.status_code == 200
-        assert response.headers["Content-Type"] == "application/json"
+        assert response.status_code == HTTPStatus.OK
+        assert response.headers["Content-Type"] == content_type
 
     def test_returns_404_when_there_are_no_module_versions(self):
         url = f"{BASE_URL}/terraform/modules/v1/X/X/X/versions"
-        response = requests.get(url, timeout=30)
+        response = requests.get(url)
         response_data = response.json()
-        assert response.status_code == 404
+        assert response.status_code == HTTPStatus.NOT_FOUND
         assert response_data == {"errors": ["Not Found"]}
 
 
@@ -31,12 +35,12 @@ class TestDownloadModuleVersion:
     def test_returns_x_terraform_get_header(self):
         url = f"{BASE_URL}/terraform/modules/v1/A/B/C/1.0.0/download"
         response = requests.get(url)
-        assert response.status_code == 204
+        assert response.status_code == HTTPStatus.NO_CONTENT
         assert response.headers["X-TERRAFORM-GET"] == ""
         assert response.headers["Content-Length"] == "0"
 
     def test_returns_404_error_when_module_version_does_not_exist(self):
         url = f"{BASE_URL}/terraform/modules/v1/A/B/C/0.0.0/download"
         response = requests.get(url)
-        assert response.status_code == 200
-        assert response.headers["Content-Type"] == "application/json"
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        assert response.headers["Content-Type"] == content_type
