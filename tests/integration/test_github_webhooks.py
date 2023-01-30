bfc0fded847d7ac3fb34b1d2e596f47578761559
George Alton <georgealton@gmail.com>
2023-01-30 00:06:05 +0000

    send content type

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -9,27 +9,39 @@ class TestGitHubWebHooks:
     def test_repository_tag_added(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-tag-added.json")
-        headers = {"X-GITHUB-EVENT": "repository"}
+        headers = {
+            "X-GITHUB-EVENT": "repository",
+            "Content-Type": "application/json",
+        }
         response = requests.post(url, data=data, headers=headers)
         assert response
 
     def test_repository_created(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-repository-created.json")
-        headers = {"X-GITHUB-EVENT": "repository"}
-        response = requests.post(url, data=data, headers=headers)
+        headers = {
+            "X-GITHUB-EVENT": "repository",
+            "Content-Type": "application/json",
+        }
+        response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
 
     def test_repository_deleted(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-repository-deleted.json")
-        headers = {"X-GITHUB-EVENT": "repository"}
-        response = requests.post(url, data=data, headers=headers)
+        headers = {
+            "X-GITHUB-EVENT": "repository",
+            "Content-Type": "application/json",
+        }
+        response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
 
     def test_repository_deleted(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-app-installed.json")
-        headers = {"X-GITHUB-EVENT": "installation"}
-        response = requests.post(url, data=data, headers=headers)
+        headers = {
+            "X-GITHUB-EVENT": "installation",
+            "Content-Type": "application/json",
+        }
+        response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
