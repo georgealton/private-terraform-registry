0e545de867ad57ad763c6425cab722ec0e4e51f7
George Alton <georgealton@gmail.com>
2023-01-30 00:16:11 +0000

    balh

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -3,45 +3,36 @@ from pathlib import Path
 import requests
 
 BASE_URL = "https://terraform.georgealton.com"
+default_headers = {
+    "Content-Type": "application/json",
+}
 
 
 class TestGitHubWebHooks:
     def test_repository_tag_added(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-tag-added.json")
-        headers = {
-            "X-GITHUB-EVENT": "repository",
-            "Content-Type": "application/json",
-        }
-        response = requests.post(url, data=data, headers=headers)
+        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
+        response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
 
     def test_repository_created(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-repository-created.json")
-        headers = {
-            "X-GITHUB-EVENT": "repository",
-            "Content-Type": "application/json",
-        }
+        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
 
     def test_repository_deleted(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-repository-deleted.json")
-        headers = {
-            "X-GITHUB-EVENT": "repository",
-            "Content-Type": "application/json",
-        }
+        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
 
     def test_repository_deleted(self):
         url = f"{BASE_URL}/webhooks/github"
         data = Path("./data/github/events/body-app-installed.json")
-        headers = {
-            "X-GITHUB-EVENT": "installation",
-            "Content-Type": "application/json",
-        }
+        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response
