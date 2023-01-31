0d038a031fe0c83e7ec5e9d2ea8952c05d0a4c0c
George Alton <georgealton@gmail.com>
2023-01-31 21:08:18 +0000

    json lines?

---
+++ tests/integration/conftest.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+import boto3
+
+
+def db_connection():
+    table = ""
+    dynamodb = boto3.resource("dynamodb")
+    yield dynamodb.Table(table)
+
+
+def provisioner(table):
+    items = Path("data/dynamodb/items.jsonl")
+    with items.open() as items_file:
+        with table.batch_writer() as batch:
+            for json_line in items_file:
+                item = json.loads(json_line)
+                batch.put_item(Item=item)
+        yield
