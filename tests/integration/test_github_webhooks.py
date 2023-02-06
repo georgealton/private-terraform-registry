8a7bd78ba37a8c9e13effafe2c85ae2b2b8ffdf2
George Alton <georgealton@gmail.com>
2023-02-06 10:48:17 +0000

    use urljoin

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from urllib.parse import urljoin
 
 import pytest
 import requests
@@ -12,28 +13,28 @@ default_headers = {
 @pytest.mark.xfail(reason="Not Implemented")
 class TestGitHubWebHooks:
     def test_repository_tag_added(self):
-        url = f"{BASE_URL}/webhooks/github"
+        url = urljoin(BASE_URL, "/webhooks/github")
         data = Path("./data/github/events/body-tag-added.json")
         headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
 
     def test_repository_created(self):
-        url = f"{BASE_URL}/webhooks/github"
+        url = urljoin(BASE_URL, "/webhooks/github")
         data = Path("./data/github/events/body-repository-created.json")
         headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
 
     def test_repository_deleted(self):
-        url = f"{BASE_URL}/webhooks/github"
+        url = urljoin(BASE_URL, "/webhooks/github")
         data = Path("./data/github/events/body-repository-deleted.json")
         headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
 
     def test_repository_deleted(self):
-        url = f"{BASE_URL}/webhooks/github"
+        url = urljoin(BASE_URL, "/webhooks/github")
         data = Path("./data/github/events/body-app-installed.json")
         headers = default_headers | {"X-GITHUB-EVENT": "repository"}
         response = requests.post(url, data=data.read_text(), headers=headers)
--- tests/integration/test_module_api.py
+++ tests/integration/test_module_api.py
@@ -1,15 +1,17 @@
 from mimetypes import types_map
 from http import HTTPStatus
+from urllib.parse import urljoin
+
 import requests
 
 BASE_URL = "https://terraform.georgealton.com"
-MODULES_V1_URL = f"{BASE_URL}/terraform/modules/v1/"
+MODULES_V1_URL = urljoin(BASE_URL, "terraform/modules/v1/")
 CONTENT_TYPE = types_map[".json"]
 
 
 class TestServiceDiscovery:
     def test_returns_modules_url(self):
-        url = f"{BASE_URL}/.well-known/terraform.json"
+        url = urljoin(BASE_URL, ".well-known/terraform.json")
         response = requests.get(url)
         body = response.json()
         assert response.status_code == HTTPStatus.OK
@@ -19,7 +21,7 @@ class TestServiceDiscovery:
 
 class TestListModuleVersions:
     def test_returns_a_list_of_versions(self):
-        url = f"{MODULES_V1_URL}A/B/C/versions"
+        url = urljoin(MODULES_V1_URL, "A/B/C/versions")
         response = requests.get(url)
         body = response.json()
         assert response.status_code == HTTPStatus.OK
@@ -27,7 +29,7 @@ class TestListModuleVersions:
         assert body == {"modules": [{"versions": [{"version": "1.0.0"}]}]}
 
     def test_returns_404_when_there_are_no_module_versions(self):
-        url = f"{MODULES_V1_URL}X/X/X/versions"
+        url = urljoin(MODULES_V1_URL, "X/X/X/versions")
         response = requests.get(url)
         response_data = response.json()
         assert response.status_code == HTTPStatus.NOT_FOUND
@@ -36,14 +38,14 @@ class TestListModuleVersions:
 
 class TestDownloadModuleVersion:
     def test_returns_x_terraform_get_header(self):
-        url = f"{MODULES_V1_URL}A/B/C/1.0.0/download"
+        url = urljoin(MODULES_V1_URL, "A/B/C/1.0.0/download")
         response = requests.get(url)
         assert response.status_code == HTTPStatus.NO_CONTENT
         assert response.headers["X-TERRAFORM-GET"] == "https://blah.com"
         assert response.headers["Content-Length"] == "0"
 
     def test_returns_404_error_when_module_version_does_not_exist(self):
-        url = f"{MODULES_V1_URL}A/B/C/0.0.0/download"
+        url = urljoin(MODULES_V1_URL, "A/B/C/0.0.0/download")
         response = requests.get(url)
         assert response.status_code == HTTPStatus.NOT_FOUND
         assert response.headers["Content-Type"] == CONTENT_TYPE
