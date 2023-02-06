53a8c5b1a992e11df1f74d78d58a8aea518d28fb
George Alton <georgealton@gmail.com>
2023-02-06 10:52:57 +0000

    extract for readabilty

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -15,27 +15,31 @@ class TestGitHubWebHooks:
     def test_repository_tag_added(self):
         url = urljoin(BASE_URL, "webhooks/github")
         data = Path("./data/github/events/body-tag-added.json")
-        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
+        github_event_header = {"X-GITHUB-EVENT": "repository"}
+        headers = default_headers | github_event_header
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
 
     def test_repository_created(self):
         url = urljoin(BASE_URL, "webhooks/github")
         data = Path("./data/github/events/body-repository-created.json")
-        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
+        github_event_header = {"X-GITHUB-EVENT": "repository"}
+        headers = default_headers | github_event_header
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
 
     def test_repository_deleted(self):
         url = urljoin(BASE_URL, "webhooks/github")
         data = Path("./data/github/events/body-repository-deleted.json")
-        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
+        github_event_header = {"X-GITHUB-EVENT": "repository"}
+        headers = default_headers | github_event_header
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
 
     def test_repository_deleted(self):
         url = urljoin(BASE_URL, "webhooks/github")
         data = Path("./data/github/events/body-app-installed.json")
-        headers = default_headers | {"X-GITHUB-EVENT": "repository"}
+        github_event_header = {"X-GITHUB-EVENT": "repository"}
+        headers = default_headers | github_event_header
         response = requests.post(url, data=data.read_text(), headers=headers)
         assert response.status_code == 204
