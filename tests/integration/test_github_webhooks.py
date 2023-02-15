85aabeb2b2b3283e94a6da948a128b98b8f560dd
George Alton <georgealton@gmail.com>
2023-02-15 09:47:58 +0000

    asdasd

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -1,7 +1,5 @@
-from pathlib import Path
 from urllib.parse import urljoin
 
-import pytest
 import requests
 
 BASE_URL = "https://terraform.georgealton.com"
@@ -12,8 +10,8 @@ default_headers = {
 
 class TestGitHubWebHooks:
     def test_app_installed(self, request):
-        path = "webhooks/github"
-        url = urljoin(BASE_URL, path)
+        endpoint = "webhooks/github"
+        url = urljoin(BASE_URL, endpoint)
         data = request.path.parent / "data/github/events/body-app-installation.json"
         github_event_header = {"X-GITHUB-EVENT": "repository"}
         headers = default_headers | github_event_header
@@ -23,8 +21,8 @@ class TestGitHubWebHooks:
         assert response.status_code == 202
 
     def test_repository_tag_added(self, request):
-        path = "webhooks/github"
-        url = urljoin(BASE_URL, path)
+        endpoint = "webhooks/github"
+        url = urljoin(BASE_URL, endpoint)
         data = request.path.parent / "data/github/events/body-tag-added.json"
         github_event_header = {"X-GITHUB-EVENT": "repository"}
         headers = default_headers | github_event_header
@@ -34,8 +32,8 @@ class TestGitHubWebHooks:
         assert response.status_code == 202
 
     def test_repository_created(self, request):
-        path = "webhooks/github"
-        url = urljoin(BASE_URL, path)
+        endpoint = "webhooks/github"
+        url = urljoin(BASE_URL, endpoint)
         data = request.path.parent / "data/github/events/body-repository-created.json"
         github_event_header = {"X-GITHUB-EVENT": "repository"}
         headers = default_headers | github_event_header
@@ -45,8 +43,8 @@ class TestGitHubWebHooks:
         assert response.status_code == 202
 
     def test_repository_deleted(self, request):
-        path = "webhooks/github"
-        url = urljoin(BASE_URL, path)
+        endpoint = "webhooks/github"
+        url = urljoin(BASE_URL, endpoint)
         data = request.path.parent / "data/github/events/body-repository-deleted.json"
         github_event_header = {"X-GITHUB-EVENT": "repository"}
         headers = default_headers | github_event_header
