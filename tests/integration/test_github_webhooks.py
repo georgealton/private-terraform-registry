7032f2faa4093897df7cc3c02da86f9b90d31f12
George Alton <georgealton@gmail.com>
2023-02-15 09:56:28 +0000

    sadasd

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -1,54 +1,58 @@
 from urllib.parse import urljoin
+from pathlib import Path
 
 import requests
 
 BASE_URL = "https://terraform.georgealton.com"
-default_headers = {
-    "Content-Type": "application/json",
-}
+DEFAULT_HEADERS = {"Content-Type": "application/json"}
+EVENT_DATA = Path("data/github/events")
 
 
 class TestGitHubWebHooks:
     def test_app_installed(self, request):
         endpoint = "webhooks/github"
         url = urljoin(BASE_URL, endpoint)
-        data = request.path.parent / "data/github/events/body-app-installation.json"
+        data_path = request.path.parent / EVENT_DATA / "body-app-installation.json"
+        data = data_path.read_bytes()
         github_event_header = {"X-GITHUB-EVENT": "repository"}
-        headers = default_headers | github_event_header
-        response = requests.post(
-            url, data=data.read_text(), headers=headers, timeout=30
-        )
+        headers = DEFAULT_HEADERS | github_event_header
+
+        response = requests.post(url, data=data, headers=headers, timeout=30)
+
         assert response.status_code == 202
 
     def test_repository_tag_added(self, request):
         endpoint = "webhooks/github"
         url = urljoin(BASE_URL, endpoint)
-        data = request.path.parent / "data/github/events/body-tag-added.json"
+        data_path = request.path.parent / EVENT_DATA / "body-tag-added.json"
+        data = data_path.read_bytes()
         github_event_header = {"X-GITHUB-EVENT": "repository"}
-        headers = default_headers | github_event_header
-        response = requests.post(
-            url, data=data.read_text(), headers=headers, timeout=30
-        )
+        headers = DEFAULT_HEADERS | github_event_header
+
+        response = requests.post(url, data=data, headers=headers, timeout=30)
+
         assert response.status_code == 202
 
     def test_repository_created(self, request):
         endpoint = "webhooks/github"
         url = urljoin(BASE_URL, endpoint)
-        data = request.path.parent / "data/github/events/body-repository-created.json"
+        data_path = request.path.parent / EVENT_DATA / "body-repository-created.json"
+        data = data_path.read_bytes()
         github_event_header = {"X-GITHUB-EVENT": "repository"}
-        headers = default_headers | github_event_header
-        response = requests.post(
-            url, data=data.read_text(), headers=headers, timeout=30
-        )
+        headers = DEFAULT_HEADERS | github_event_header
+
+        response = requests.post(url, data=data, headers=headers, timeout=30)
+
         assert response.status_code == 202
 
     def test_repository_deleted(self, request):
         endpoint = "webhooks/github"
         url = urljoin(BASE_URL, endpoint)
-        data = request.path.parent / "data/github/events/body-repository-deleted.json"
+        data_path = request.path.parent / EVENT_DATA / "body-repository-deleted.json"
+        data = data_path.read_bytes()
         github_event_header = {"X-GITHUB-EVENT": "repository"}
-        headers = default_headers | github_event_header
-        response = requests.post(
-            url, data=data.read_text(), headers=headers, timeout=30
-        )
+        headers = DEFAULT_HEADERS | github_event_header
+
+        response = requests.post(url, data=data, headers=headers, timeout=30)
+
         assert response.status_code == 202
