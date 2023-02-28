9e7073f5f9f308888ec1cf4cfaba3c84eeb4e4f8
George Alton <georgealton@gmail.com>
2023-02-28 00:06:17 +0000

    parametrize github tests

--- tests/integration/test_github_webhooks.py
+++ tests/integration/test_github_webhooks.py
@@ -1,13 +1,15 @@
 from urllib.parse import urljoin
 from pathlib import Path
+from typing import Any
 
+import pytest
 import requests
 
 DEFAULT_HEADERS = {"Content-Type": "application/json"}
 EVENT_DATA = Path("data/github/events")
 
 
-def prepare_post(request, parameters, event_filename, event_type):
+def prepare_post(request, parameters, event_filename, event_type) -> tuple[str, dict[str, Any], dict[str, str]]:
     host = f"{parameters['DomainName']}.{parameters['ParentDomain']}"
     endpoint = "webhooks/github"
     url = urljoin(f"https://{host}", endpoint)
@@ -19,26 +21,13 @@ def prepare_post(request, parameters, event_filename, event_type):
 
 
 class TestGitHubWebHooks:
-    def test_app_installed(self, parameters, request):
-        filename = "body-app-installation.json"
-        url, data, headers = prepare_post(request, parameters, filename, "repository")
-        response = requests.post(url, data=data, headers=headers, timeout=30)
-        assert response.status_code == 202
-
-    def test_repository_tag_added(self, parameters, request):
-        filename = "body-tag-added.json"
-        url, data, headers = prepare_post(request, parameters, filename, "repository")
-        response = requests.post(url, data=data, headers=headers, timeout=30)
-        assert response.status_code == 202
-
-    def test_repository_created(self, parameters, request):
-        filename = "body-repository-created.json"
-        url, data, headers = prepare_post(request, parameters, filename, "repository")
-        response = requests.post(url, data=data, headers=headers, timeout=30)
-        assert response.status_code == 202
-
-    def test_repository_deleted(self, parameters, request):
-        filename = "body-repository-deleted.json"
+    @pytest.mark.parametrize('filename', [
+        "body-repository-deleted.json",
+        "body-repository-created.json",
+        "body-tag-added.json",
+        "body-app-installation.json",
+    ])
+    def test_app_installed(self, parameters, request, filename):
         url, data, headers = prepare_post(request, parameters, filename, "repository")
         response = requests.post(url, data=data, headers=headers, timeout=30)
         assert response.status_code == 202
